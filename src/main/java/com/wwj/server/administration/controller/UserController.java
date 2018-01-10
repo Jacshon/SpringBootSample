7cc54dfa734aa12a2ea9e93514293cdc6c6e6c5e
weijiang wang <wangweijiang1990@sina.com>
2018-01-10 22:31:25 +0800

    Controller 修改

--- src/main/java/com/wwj/server/administration/controller/UserController.java
+++ src/main/java/com/wwj/server/administration/controller/UserController.java
@@ -2,6 +2,7 @@ package com.wwj.server.administration.controller;
 
 import com.wwj.server.administration.domain.User;
 import com.wwj.server.administration.service.UserService;
+import com.wwj.server.common.domain.Response;
 import com.wwj.server.shared.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,18 +21,19 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = URL.USER_SEARCH, method = RequestMethod.GET)
-    public List<User> userlist() {
-        return userService.userList();
+    public Response userlist() {
+        List<User> userList = userService.userList();
+        return Response.ok(userService.userList());
     }
 
     @RequestMapping(value = URL.USER_SAVE, method = RequestMethod.POST)
-    public User userSave(@RequestBody User user) {
-        return userService.userSave(user);
+    public Response userSave(@RequestBody User user) {
+        return Response.ok(userService.userSave(user));
     }
 
     @PostMapping(value = URL.USER_DETAIL)
-    public void userDetail(@PathVariable("userId") String userId) throws Exception{
-        userService.getUserDetails(userId);
+    public Response userDetail(@PathVariable("userId") String userId) throws Exception{
+        return Response.ok(userService.getUserDetails(userId));
     }
 
 }
