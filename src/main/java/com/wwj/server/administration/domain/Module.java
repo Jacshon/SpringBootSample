a990924270f520201bc0fcb948aff4d96468145f
weijiang wang <wangweijiang1990@sina.com>
2018-01-27 22:05:36 +0800

    添加模块

---
+++ src/main/java/com/wwj/server/administration/domain/Module.java
@@ -0,0 +1,80 @@
+package com.wwj.server.administration.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sys_module")
+public class Module {
+    @Column(name = "module_id", length = 32)
+    private String moduleId;
+    @Column(name = "name", length = 64)
+    private String name;
+    @Column(name = "code", length = 32)
+    private String code;
+    @Column(name = "priority", length = 11)
+    private Integer priority;
+    @Column(name = "status", length = 2)
+    private String status;
+    @Column(name = "create_time")
+    private Long createTime;
+    @Column(name = "update_time")
+    private Long updateTime;
+
+    public String getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(String moduleId) {
+        this.moduleId = moduleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
+}
